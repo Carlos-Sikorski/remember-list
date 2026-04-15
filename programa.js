db420a8e413916b1616b9e4f3f3cd58c50c5e131
Carlos Eduardo <cadusikorski21@gmail.com>
2026-04-14 21:36:39 -0300

    criação do readline e do menu de integração

---
+++ programa.js
@@ -0,0 +1,42 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let lembretes = [];
+
+function menu() {
+
+    console.log("**SEJA BEM VINDO AO MENU**")
+    rl.question("Digite a opção desejada[SOMENTE NUMEROS]\n[1]-Cadastrar Lembrete [2]-Listar Lembretes [3]-Editar Lembretes\n[4]-Marcar Concluído [5]-Excluir Lembretes [0]-Sair\nR:", (input) => {
+
+        switch (i) {
+
+            case '1': cadastrarLembrete();
+                break;
+
+            case '2': listarLembretes();
+                break;
+
+            case '3': editarLembretes();
+                break;
+
+            case '4': marcarConcluido();
+                break;
+
+            case '5': excluirLembrete();
+                break;
+
+            case '0': console.log("Até a próxima!")
+                rl.close()
+
+            default: console.log("Não entendi!\nDigite um número válido!")
+                menu()
+        }
+
+    })
+
+};
+
+menu()
